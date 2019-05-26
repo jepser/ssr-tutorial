51c4ebde03854a21dbd9984ba484e4ed72c337c4
Jepser Bernardino <jepsersk8@gmail.com>
2019-05-26 13:27:14 +0200

    feat: add ssr to styles

--- src/server/index.js
+++ src/server/index.js
@@ -8,14 +8,16 @@ const server = express()
 server.use(express.static(path.resolve(__dirname, '../../build')))
 
 server.get('/*', async (req, res) => {
-  const mainFile = path.resolve(__dirname, '../../build/index.html')
-  const { html, initialData } = await render(req)
+  const mainFile = path.resolve(__dirname, '../../build/main.html')
+  const { html, initialData, styles } = await render(req)
+
+  console.log('on handler', req.url, html)
   fs.readFile(mainFile, 'utf8', (err, data) => {
     if (err) {
       console.error('faaaak', err)
     }
 
-    return res.send(htmlTemplate(data, { html, initialData }))
+    return res.send(htmlTemplate(data, { html, initialData, styles }))
   })
 })
 
@@ -23,7 +25,8 @@ server.listen(3000, () => {
   console.log('server listirigillo')
 })
 
-function htmlTemplate (template, { html, initialData = {} }) {
+function htmlTemplate (template, { html, styles, initialData = {} }) {
   const withInitialData = template.replace('{INITIAL_DATA}', JSON.stringify(initialData))
-  return withInitialData.replace('<div id="app"></div>', `<div id="app">${html}</div>`)
+  const withStyles = withInitialData.replace('{STYLES}', styles)
+  return withStyles.replace('<div id="app"></div>', `<div id="app">${html}</div>`)
 }
